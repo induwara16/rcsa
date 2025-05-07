de70f717f2f52ed725c64b680a6e6bd52c25ac2b
Induwara <induwara16@yandex.com>
2025-05-08 01:38:45 +0530

    hero kinda done

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,16 +26,22 @@ const theme = createTheme({
       list: '*:!mx-0 md:h-full *:md:h-full *:md:flex *:md:items-center *:hover:!bg-gray-100 *:dark:hover:!bg-gray-700',
     },
     link: {
-      base: "text-base font-semibold hover:!text-sky-600 hover:dark:!text-sky-300 flex !px-4 !mx-0",
+      base: "text-base font-semibold hover:!text-sky-600 hover:dark:!text-sky-400 flex !px-4 !mx-0",
       active: {
-        on: "text-sky-700 dark:text-sky-400",
+        on: "text-sky-700 dark:text-sky-500",
       }
     }
   },
   dropdown: {
     floating: {
-      item: { base: '!text-base hover:!text-sky-600 hover:dark:!text-sky-300' }
+      item: { base: '!text-base hover:!text-sky-600 hover:dark:!text-sky-400' }
     },
+  },
+  button: {
+    outlineColor: {
+      default: 'border border-neutral-800 text-neutral-800 hover:border-primary-800 hover:bg-primary-800 hover:text-white focus:ring-primary-300 dark:border-neutral-300 dark:text-neutral-300 dark:hover:border-primary-700 dark:hover:bg-primary-700 dark:hover:text-white dark:focus:ring-primary-800'
+    },
+    base: 'py-6 px-7 tracking-wide'
   }
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+import { Button } from "flowbite-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="flex max-md:flex-col items-center text-black/80 dark:text-white bg-white dark:bg-gray-800 px-10 min-h-[100vh] max-md:text-center my-10 md:-mt-16">
-      <div className="flex-1/2 flex flex-col gap-5">
+    <section className="flex max-md:flex-col items-center text-black/80 dark:text-white bg-white dark:bg-gray-800 px-10 min-h-[100vh] max-md:text-center py-10 md:-mt-16">
+      <div className="flex-1/2 flex flex-col gap-10">
         <h1 className="text-4xl md:text-5xl font-semibold leading-12 md:leading-15">Royal College Science Association</h1>
-        <p className="mt-4 text-base leading-7 text-black/60 dark:text-white/60">
+        <p className="text-base leading-7 text-black/60 dark:text-white/60">
           Explore the mysteries of science with the Royal College Science Association, join us now and expand your knowledge
         </p>
+
+        <div className="flex max-sm:flex-col gap-5 mt-4">
+          <Button pill as={Link} href="/about-us">View More</Button>
+          <Button pill as={Link} href="/register" outline>Join Us</Button>
+        </div>
       </div>
       <div className="flex-1/2"></div>
     </section>
